3a071f1eb10dfe28e0bc8b96636d75542338cb2d
Licho <elielic2000@gmail.com>
2024-09-08 22:51:33 -0600

    Practica de python

--- hellogit3.py
+++ hellogit3.py
@@ -9,4 +9,25 @@ print(c)
 
 print("Mi nombre es", "Python.", end=" ")
 print("Monty Python.")
+#Programming***Essentials***in...Python#
+print("Programming","Essentials","in", sep="***", end="",)
+print("","Python.", sep="...")
 
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+
+
+
+def new_func():
+    print("Mi\nnombre\nes\nBond.", end=" ")
+    print("James Bond.")
+
+new_func()
+
+print(0x129)
+
+print("\"Estoy'Aprendiendo\"\n\"\"\"\n\"\"\"Python\"\"\"")
+
+
+
+ 
